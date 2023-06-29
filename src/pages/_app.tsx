636921b6e20097a5bf73bacd89ecbc3c83b76ff9
romain.piveteau <r.piveteau@outlook.com>
2023-06-29 20:00:02 +0200

    Fix ts confiig right

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,5 +3,6 @@ import "../public/main.css"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: any) {
+  
   return <Component {...pageProps} />
 }
