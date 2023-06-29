650631ef431f28719f0f9448048ac1634feb8ae0
romain.piveteau <r.piveteau@outlook.com>
2023-06-29 10:11:33 +0200

    toto

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,9 @@ dotenv.config()
 import Server from './server'
 
 const begin = async () => {
+  
   await new Server(process.env.EXPRESS_PORT).start()
+  
   console.log(
     `Server running in --- ${process.env.NODE_ENV} --- on port ${process.env.EXPRESS_PORT}`,
   )
