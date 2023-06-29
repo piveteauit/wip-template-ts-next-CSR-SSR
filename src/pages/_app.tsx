b35c833e3d064e569dc6c515608089a76627feea
romain.piveteau <r.piveteau@outlook.com>
2023-06-29 20:01:27 +0200

    toto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,5 @@ import "../public/main.css"
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: any) {
-  
   return <Component {...pageProps} />
 }
