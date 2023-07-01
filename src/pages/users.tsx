c78e5826d675188bf6f125fdc8799aa268fa3909
romain.piveteau <r.piveteau@outlook.com>
2023-07-01 21:57:02 +0200

    Reinstall tailwind

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,9 +1,21 @@
-import e from "express";
 import { useEffect, useState } from "react";
+import Table from '../view/table';
+
+const data = [
+    { id: 1, name: "John Doe", age: 30 },
+    { id: 2, name: "Jane Doe", age: 25 },
+    { id: 3, name: "Peter Smith", age: 40 },
+];
+
+const columns = [
+    { id: "id", name: "ID" },
+    { id: "name", name: "Name" },
+    { id: "age", name: "Age" },
+];
 
 export default function Users (props: any) {
     const [users, setUsers] = useState([]);
-    
+
     useEffect(() => {
         fetch("/api/users")
             .then(r => r.json())
@@ -11,13 +23,17 @@ export default function Users (props: any) {
     }, [])
 
     return (
-        <ul>
-            { users.map((u, i) => 
-                <li key={i}>
-                    {u.id} - {u.name}
-                </li>
-            ) }
-        </ul>
+        <div>
+            <ul>
+                { users.map((u, i) =>
+                  <li key={i}>
+                      {u.id} - {u.name}
+                  </li>
+                ) }
+            </ul>
+
+            <Table data={data} columns={columns} />
+        </div>
     )
 
 }
--- src/view/layout.tsx
+++ src/view/layout.tsx
@@ -1,14 +1,13 @@
 import React from "react"
 import Head from "next/head"
-
 const renderBack = (backPath: any) => {
   return (
-    <a style={{ position: "fixed", fontSize: "3vh" }} href={backPath}>
-      {" "}
-      Back{" "}
+    <a style={{position: 'fixed', fontSize: '3vh'}} href={backPath}>
+      {' '}
+      Back{' '}
     </a>
-  )
-}
+  );
+};
 
 export default function Layout(props: any) {
   return (
