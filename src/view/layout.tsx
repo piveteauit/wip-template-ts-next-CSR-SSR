13196303e3a4e232572c5fedb3e3624efe6ac2df
romain.piveteau <r.piveteau@outlook.com>
2023-07-01 22:30:14 +0200

    Fix precommit

--- src/view/layout.tsx
+++ src/view/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import Head from "next/head"
 
+
 const renderBack = (backPath: any) => {
   return (
     <a style={{ position: "fixed", fontSize: "3vh" }} href={backPath}>
